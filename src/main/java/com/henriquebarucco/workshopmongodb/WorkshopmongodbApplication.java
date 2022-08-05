b731461e1959f6e41466852df08cdba1755263d6
Henrique Barucco <henrique_barucco@hotmail.com>
2022-08-05 00:01:00 -0300

    Entity User & Rest

--- src/main/java/com/henriquebarucco/workshopmongodb/WorkshopmongodbApplication.java
+++ src/main/java/com/henriquebarucco/workshopmongodb/WorkshopmongodbApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WorkshopmongodbApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WorkshopmongodbApplication.class, args);
     }
-
 }
