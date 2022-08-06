1b29d38c185c468d51b30cd8107aa96857ff55ff
Henrique Barucco <henrique_barucco@hotmail.com>
2022-08-06 17:54:55 -0300

    Update User

--- src/main/java/com/henriquebarucco/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/henriquebarucco/workshopmongodb/resources/UserResource.java
@@ -46,4 +46,13 @@ public class UserResource {
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id) {
+        User obj = userService.fromDTO(objDTO);
+        obj.setId(id);
+        obj = userService.update(obj);
+
+        return ResponseEntity.noContent().build();
+    }
 }
